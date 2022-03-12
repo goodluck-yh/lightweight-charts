9cd5d4d566474aa0a80fc67c1b53b7eeaab8acd4
Henry Yang <goodluck.yh2014@gmail.com>
2022-03-13 10:32:40 +1100

    Add log to debug npm install issue

--- scripts/githooks/install.js
+++ scripts/githooks/install.js
@@ -13,10 +13,12 @@ function fixPath(p) {
 
 function getGitRoot() {
 	let directory = fs.realpathSync(__dirname);
+	console.log(`${magentaColor}directory is ${directory}${noColor}`);
 
 	// eslint-disable-next-line no-constant-condition
 	while (true) {
 		const gitDir = path.join(directory, '.git');
+		console.log(`${magentaColor}gitDir is ${gitDir}${noColor}`);
 		if (fs.existsSync(gitDir) && fs.statSync(gitDir).isDirectory()) {
 			return fixPath(directory);
 		}
@@ -47,7 +49,9 @@ fi`;
 }
 
 function main() {
+	console.log(`${magentaColor}__dirname is ${__dirname}${noColor}`);
 	const gitRoot = getGitRoot();
+	console.log(`${magentaColor}gitRoot is ${gitRoot}${noColor}`);
 	if (gitRoot === null) {
 		console.error(`${redColor}It seems that it isn't a git repo. Did you use git to clone the repo? Skip installing git-hooks${noColor}`);
 		return;
@@ -61,21 +65,21 @@ function main() {
 		fs.mkdirSync(hooksDir, { recursive: true });
 	}
 
-	// for (const hookName of ['pre-commit']) {
-	// 	const hookEntry = fixPath(path.join(hooksDir, hookName));
-	//
-	// 	try {
-	// 		// try to unlink first: avoid writing thru a symlink
-	// 		fs.unlinkSync(hookEntry);
-	// 	} catch (e) {
-	// 		// do nothing
-	// 	}
-	//
-	// 	const relativePath = fixPath(path.join(targetDir, hookName));
-	// 	fs.writeFileSync(hookEntry, getHookScript(relativePath), { encoding: 'utf-8' });
-	//
-	// 	fs.chmodSync(hookEntry, '0755');
-	// }
+	for (const hookName of ['pre-commit']) {
+		const hookEntry = fixPath(path.join(hooksDir, hookName));
+
+		try {
+			// try to unlink first: avoid writing thru a symlink
+			fs.unlinkSync(hookEntry);
+		} catch (e) {
+			// do nothing
+		}
+
+		const relativePath = fixPath(path.join(targetDir, hookName));
+		fs.writeFileSync(hookEntry, getHookScript(relativePath), { encoding: 'utf-8' });
+
+		fs.chmodSync(hookEntry, '0755');
+	}
 
 	console.log(`${magentaColor}Installed to ${gitRoot}${noColor}`);
 }
